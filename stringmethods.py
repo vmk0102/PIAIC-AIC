09b4837994b35224f0d32f2ac404a47e8f06b3c9
wali <valimohammadkhubaib@gmail.com>
2019-05-26 13:15:34 +0500

    Assignment 1 PYTHON

---
+++ stringmethods.py
@@ -0,0 +1,30 @@
+#**************ASSIGNMENT 1***************
+
+
+a = "my name is Wali"
+b = "PYTHON IS GREAT!"
+c = "Hello World"
+num = "6789325"
+
+
+print(a.capitalize())
+print(c.lower())
+print(b.casefold())
+print(a.find("is"))
+print(a.count(""))
+print(b.istitle())
+print(a.isalnum())
+print(a.endswith("li"))
+print(b.replace("GREAT", "BAD"))
+print(b.encode())
+print(c.join("XYZ"))
+print(c.swapcase())
+print(a.partition("is"))
+print(b.rpartition("E"))
+print(c.startswith("Hell"))
+print(int(num))
+print(float(num))
+print(len(a))
+print(max(a))
+print(min(num))
+print(sorted(b))
